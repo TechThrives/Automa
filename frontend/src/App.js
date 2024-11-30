4822bc616e8d8fb4b48ace5a30f99d9fe14808a7
OmkarKanade <omkarkanade8888@gmail.com>
2024-11-30 13:22:31 +0530

    feat: auth pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ConnectAuth from "./pages/dashboard/ConnectAuth";
 import Workflow from "./pages/dashboard/Workflow";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 
 function App() {
   return (
     <Router>
         <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard/connect" element={<ConnectAuth />} />
           <Route path="/dashboard/workflow/:id" element={<Workflow />} />
         </Routes>
